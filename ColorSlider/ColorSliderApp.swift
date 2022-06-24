57a0f343d3a8848fbbe741f6a920e5580d216138
moritamu <moris@r.sannet.ne.jp>
2022-06-24 14:29:18 +0900

    Initial Commit

---
+++ ColorSlider/ColorSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderApp.swift
+//  ColorSlider
+//
+//  Created by 森田晋 on 2022/06/24.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
